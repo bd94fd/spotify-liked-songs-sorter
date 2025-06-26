4c4e0adf18fdcfdae7f9441f1e8bee3ae939ebae
bd94fd <bobbymichaeldean@gmail.com>
2025-06-26 14:43:21 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const CLIENT_ID = 'a054f7c69479463e9987dcedfe9aeedf';
+export const REDIRECT_URI = 'https://your-deployment-url-here/'; // Replace with your deployment URL ending with /
+export const SCOPES = [
+  'user-library-read',
+  'playlist-modify-private',
+  'playlist-modify-public',
+  'playlist-read-private',
+];
